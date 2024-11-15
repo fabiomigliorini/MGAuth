5ef3ca2c35cb33187b94f318de72b87464276c4e
Fabio Migliorini <fabio@mgpapelaria.com.br>
2024-11-15 14:25:22 -0400

    [FIX] Layout Login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,47 +1,25 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-6">
-                <div class="card-body">
-                    <form method="POST" action="{{ route('auth') }}">
-                        <div class="form-group row">
-                            <div class="col-md-12 text-center">
-                                <h2 class="font-weight-bold">Login</h2>
-                                <!-- <h2 class="font-weight-bold">{{ $redirect_uri }}</h2> -->
-                                
-                            </div>
-                        </div>
-                        @csrf
-                        <div class="form-group row">
-                            <label for="usuario" class="col-md-4 col-form-label text-md-right">{{ __('Usuário') }}</label>
-                            <div class="col-md-6">
-                                <input id="username" type="text" class="form-control" name="username" autofocus>
-                            </div>
-                        </div>
-                        <div class="form-group row">
-                            <label for="senha" class="col-md-4 col-form-label text-md-right">{{ __('Senha') }}</label>
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password">
-                                @if(!empty($error))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>Usuário ou senha inválidos</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                        <input type="text" name="redirect_uri" value="{{ $redirect_uri }}" hidden>
-                        </div>
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-8">
-                                <button type="submit" class="btn btn-warning">
-                                    {{ __('Login') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
+    <div class="align-middle" style="height: 95vh; align-items: center; justify-content:center; display:flex">
+        <div style="max-width:300px">
+            <form method="POST" action="{{ route('auth') }}">
+                @csrf
+                <input type="text" name="redirect_uri" value="{{ $redirect_uri }}" hidden>
+                <div class="form-group">
+                    <label for="usuario">{{ __('Usuário') }}</label>
+                    <input id="username" class="form-control" type="text" name="username" autofocus>
                 </div>
+                <div class="form-group">
+                    <label for="senha">{{ __('Senha') }}</label>
+                    <input id="password" type="password" class="form-control" name="password">
+                    @if (!empty($error))
+                        <small id="emailHelp" class="form-text text-danger">Usuário ou senha inválidos</small>
+                    @endif
+                </div>
+                <button type="submit" class="btn btn-warning">
+                    {{ __('Login') }}
+                </button>
+            </form>
         </div>
     </div>
-</div>
 @endsection
