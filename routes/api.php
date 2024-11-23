925c504c17e575e8479d75098b3579f7dbbed8b7
Editado no saturno.mgpapelaria.com.br <suporte@mgpapelaria.com.br>
2024-11-23 13:09:00 -0400

    [FIX] TOO MANY REDIRECTS - throttle baixo para o check-token

--- routes/api.php
+++ routes/api.php
@@ -11,13 +11,20 @@ header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 //header('Access-Control-Allow-Origin: https://*.mgpapelaria.com.br');
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => 'throttle:api'], function ($router) {
+
+    Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('oauth/token', [AuthController::class, 'getToken'])->name('auth');
+    Route::post('oauth/token/json', [AuthController::class, 'getTokenJson']);
+    Route::post('refresh', [AuthController::class, 'refreshToken']);
+    Route::post('logout', [AuthController::class, 'logout'] )->middleware('auth:api');
 });
 
+//Throttle 1000 requisicoes por minuto
+Route::group(['middleware' => ['throttle:600,1']], function ($router) {
+    Route::get('check-token', [AuthController::class, 'checkToken']);
+});
 
-Route::post('oauth/token', [AuthController::class, 'getToken'])->name('auth');
-Route::post('oauth/token/json', [AuthController::class, 'getTokenJson']);
-Route::post('refresh', [AuthController::class, 'refreshToken']);
-Route::get('check-token', [AuthController::class, 'checkToken'])->middleware('auth:api');
-Route::post('logout', [AuthController::class, 'logout'] )->middleware('auth:api');
